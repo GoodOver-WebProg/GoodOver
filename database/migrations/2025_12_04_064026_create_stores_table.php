63ff7e12c2c44fb7a3788c17972db1f36f9d934d
Arvin Hasim <arvin.hasim@binus.ac.id>
2025-12-05 15:11:31 +0700

    #feat migration file

---
+++ database/migrations/2025_12_04_064026_create_stores_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('stores', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('address');
+            $table->integer('contact');
+            $table->integer('opening_time');
+            $table->integer('closing_time');
+            $table->text('image_path');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('stores');
+    }
+};
---
+++ database/migrations/2025_12_04_064817_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('price');
+            $table->text('description');
+            $table->bigInteger('store_id');
+            $table->foreign('store_id')->references('id')->on('stores');
+            $table->text('image_path');
+            $table->string('status');
+            $table->integer('total_quantity');
+            $table->time('pickup_time'); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
