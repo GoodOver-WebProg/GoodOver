c163925a2471df39609b99212026dd064ef82fd1
bernarduswilliams <bernarduswilliamsantosa@gmail.com>
2025-12-08 16:34:53 +0700

    commit 1

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthenticationController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -24,3 +25,6 @@ Route::prefix('/login')->group(function () {
         return view('pages.login');
     })->name('route.login.view');
 });
+
+Route::get('/profile', [ProfileController::class, 'showProfile'])
+     ->name('route.profile.view');
