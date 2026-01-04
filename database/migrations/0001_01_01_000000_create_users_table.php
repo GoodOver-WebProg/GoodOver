67d1dc808c73f996af7bf90e48b3a5a279efa345
kyooz <bernarduswilliam.santosa@gmail.com>
2026-01-04 11:19:16 +0700

    fixing update profile picture handling

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
-            $table->text('image_path'); 
+            $table->text('image_path')->nullable();
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
